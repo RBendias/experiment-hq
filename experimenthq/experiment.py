b02695da7a26ffa3321ae9f40327e680a45810fc
ExperimentHQ <ramona@kumo.ai>
2023-05-03 13:40:42 +0200

    Update

--- experimenthq/experiment.py
+++ experimenthq/experiment.py
@@ -36,9 +36,15 @@ class Experiment():
 
         if response.status_code != 200:
             raise Exception("Failed to report experiment start to REST API")
+        
+        # Read in page_id from response
+        response_json = response.json()
+        self.id = response_json["page_id"]
 
+
+    # I couldn't test this because the access token is not working
     def log_parameter(self, name, value):
-        post_data = {
+        data = {
             "experiment_key": self.id,
             "parameter_name": name,
             "parameter_value": value
@@ -47,10 +53,15 @@ class Experiment():
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.api_key}"
         }
-        response = requests.post(
+        response = requests.patch(
             url="http://localhost:8000/api/experiment/log-parameter/",
-            data=json.dumps(post_data),
+            data=json.dumps(data),
             headers=headers
         )
         if response.status_code != 200:
             raise Exception("Failed to log parameter to REST API")
+
+
+if __name__ == "__main__":
+    experiment = Experiment("2904e64c-eab8-4c3a-ba42-c5566d4fa229", "test")
+    experiment.log_parameter("test", "test")
