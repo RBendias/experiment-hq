be8055a8d0e8bdd240c578ae934c12f1e94b040c
ExperimentHQ <ramona@kumo.ai>
2023-05-17 03:07:02 +0200

    Update

--- src/experimenthq/experiment.py
+++ src/experimenthq/experiment.py
@@ -1,7 +1,5 @@
-import json
-from typing import List, Optional
-
 import requests
+from typing import List, Optional
 
 API_URL = "https://www.api.experiment-hq.com/"
 
@@ -31,6 +29,7 @@ class Experiment:
         self.name = name
         self.description = description
         self.tags = tags
+        self.session = requests.Session()
         self.experiment_id = self._start_experiment()
 
     def _start_experiment(self) -> str:
@@ -44,26 +43,20 @@ class Experiment:
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.api_key}",
         }
-        response = requests.post(
-            url=f"{API_URL}experiment", data=json.dumps(post_data), headers=headers
+        response = self.session.post(
+            f"{API_URL}experiment",
+            json=post_data,
+            headers=headers,
         )
-
-        # Raise exception if maximum number of experiments reached
         if response.status_code == 401:
             raise Exception("Invalid API key")
-
-        # Raise exception if maximum number of experiments reached
         elif response.status_code == 403:
             raise Exception("Maximum number of experiments reached")
-
         elif response.status_code == 404:
             raise Exception("ExperimentHQ database not found")
-
         elif response.status_code != 200:
             raise Exception("Failed to start experiment with message: " + response.text)
-
-        response_json = response.json()
-        return response_json.get("experiment_id")
+        return response.json().get("experiment_id")
 
     def log_parameter(self, name: str, value: str) -> None:
         data = {"parameter_name": name, "parameter_value": value}
@@ -71,18 +64,18 @@ class Experiment:
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.api_key}",
         }
-
-        response = requests.post(
-            url=f"{API_URL}experiments/{self.experiment_id}/parameters",
-            data=json.dumps(data),
+        response = self.session.post(
+            f"{API_URL}experiments/{self.experiment_id}/parameters",
+            json=data,
             headers=headers,
         )
-
         if response.status_code == 401:
             raise Exception("Invalid API key")
-
         elif response.status_code == 404:
             raise Exception("ExperimentHQ database not found")
-
+        elif response.status_code == 400:
+            raise Exception(
+                "Invalid parameter value or name. We currently only support text columns."
+            )
         elif response.status_code != 200:
             raise Exception("Failed to log parameter with message: " + response.text)
